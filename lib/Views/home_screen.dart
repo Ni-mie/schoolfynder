96a540af76327a5428f6e792c7420dcb589f5f7b
Ni-mie <owoeyeebun46@gmail.com>
2022-10-11 11:15:59 -1100

    few changes

--- lib/Views/home_screen.dart
+++ lib/Views/home_screen.dart
@@ -19,15 +19,17 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<DataModel>? schools = [];
   var isLoaded = false;
-  List<DataModel>? searchSchools = [];
+  List<DataModel>? searchSchoolnames = [];
+  List<DataModel>? searchSchoolcountries = [];
   final StreamController<bool> _streamController = StreamController.broadcast();
   @override
   void initState() {
     fetchschools();
-    searchSchools = [];
-    schools = [];
+    fetchcountries('Canada');
     super.initState();
   }
+
+  fetchcountries(String query) {}
 
   fetchschools() async {
     schools = await ApiService.fetchschools();
@@ -52,161 +54,147 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: primary,
-        body: StreamBuilder<Object>(
-            stream: _streamController.stream,
-            builder: (context, snapshot) {
-              return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 15.0, vertical: 30),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 30),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                    title: Text(
+                      'Universities,',
+                      style: headline1ExtraLarge(),
+                    ),
+                    subtitle: Text(
+                      'All Over The World',
+                      style: headline1(color: textLightColor.withOpacity(0.6)),
+                    ),
+                    leading: Image.network(
+                        'https://media.istockphoto.com/vectors/isolated-education-crest-clip-art-icon-vector-vector-id1269987763?s=612x612'),
+                    trailing: TextButton(
+                        onPressed: () {
+                          logOut(context);
+                        },
+                        child: Text(
+                          'Logout',
+                          style: bodyText1(color: textLightColor),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  //search
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
+                    decoration: boxBorderDecoration(borderRadius: 25),
+                    child: Row(
                       children: [
-                        ListTile(
-                          title: Text(
-                            'Universities,',
-                            style: headline1ExtraLarge(),
-                          ),
-                          subtitle: Text(
-                            'All Over The World',
-                            style: headline1(
-                                color: textLightColor.withOpacity(0.6)),
-                          ),
-                          leading: Image.network(
-                              'https://media.istockphoto.com/vectors/isolated-education-crest-clip-art-icon-vector-vector-id1269987763?s=612x612'),
-                          trailing: TextButton(
-                              onPressed: () {
-                                logOut(context);
-                              },
-                              child: Text(
-                                'Logout',
-                                style: bodyText1(color: textLightColor),
-                              )),
-                        ),
+                        const Icon(Icons.search, color: textLightColor),
                         const SizedBox(
-                          height: 20,
+                          width: 20,
                         ),
-                        //search
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 15, vertical: 0),
-                          decoration: boxBorderDecoration(borderRadius: 25),
-                          child: Row(
-                            children: [
-                              const Icon(Icons.search, color: textLightColor),
-                              const SizedBox(
-                                width: 20,
-                              ),
-                              Expanded(
-                                child: TextFormField(
-                                  decoration: const InputDecoration(
-                                      hintText: 'Search for Universities',
-                                      hintStyle:
-                                          TextStyle(color: textLightColor),
-                                      border: UnderlineInputBorder(
-                                          borderSide: BorderSide.none)),
-                                  onChanged: (value) {
-                                    setState(() {
-                                      schools = searchSchools!
-                                          .where((element) => element.country
-                                              .toString()
-                                              .toLowerCase()
-                                              .contains(value.toLowerCase()))
-                                          .toList();
-                                      _streamController.add(true);
-
-                                      schools = searchSchools!
-                                          .where((el) => el.name
-                                              .toString()
-                                              .toLowerCase()
-                                              .contains(value.toLowerCase()))
-                                          .toList();
-                                      _streamController.add(true);
-                                    });
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Visibility(
-                          visible: isLoaded,
-                          child: SizedBox(
-                              height: scHeight(context),
-                              child: ListView.builder(
-                                  shrinkWrap: false,
-                                  itemCount: schools!.length,
-                                  itemBuilder: (context, index) {
-                                    return Container(
-                                      width: scWidth(context),
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 5, vertical: 0),
-                                      margin: const EdgeInsets.only(
-                                          right: 2, left: 2, top: 4, bottom: 5),
-                                      decoration: BoxDecoration(
-                                          color: index == 0
-                                              ? const Color(0xffECF3FD)
-                                              : index == 1
-                                                  ? const Color(0xffD1E7CC)
-                                                  : index == 2
-                                                      ? const Color(0xffFFF9D7)
-                                                      : index == 3
-                                                          ? const Color(
-                                                              0xffF5F1FF)
-                                                          : index == 4
-                                                              ? const Color(
-                                                                  0xffF1DBE8)
-                                                              : const Color(
-                                                                  0xffCAE5E4),
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          boxShadow: [
-                                            BoxShadow(
-                                                blurRadius: 5,
-
-                                                // spreadRadius: 5,
-                                                color: textLightColor
-                                                    .withOpacity(0.4))
-                                          ]),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          ClipRRect(
-                                            borderRadius:
-                                                const BorderRadius.only(
-                                                    topLeft: Radius.circular(8),
-                                                    topRight:
-                                                        Radius.circular(8)),
-                                            child: Text(
-                                              schools![index].name,
-                                              style: headline1(),
-                                            ),
-                                          ),
-                                          Text(
-                                            '${schools![index].country},(${schools![index].alphaTwoCode})',
-                                            style: bodyText1(),
-                                          ),
-                                          Link(
-                                            target: LinkTarget.blank,
-                                            uri: Uri.parse(schools![index]
-                                                .webPages[0]
-                                                .toString()),
-                                            builder: (context, followLink) =>
-                                                TextButton(
-                                                    onPressed: followLink,
-                                                    child: Text(schools![index]
-                                                        .webPages[0]
-                                                        .toString())),
-                                          )
-                                        ],
-                                      ),
-                                    );
-                                  })),
-                        )
+                        StatefulBuilder(builder: (context, setNewState) {
+                          return Expanded(
+                            child: TextField(
+                              decoration: const InputDecoration(
+                                  hintText: 'Search for Universities',
+                                  hintStyle: TextStyle(color: textLightColor),
+                                  border: UnderlineInputBorder(
+                                      borderSide: BorderSide.none)),
+                              onChanged: (value) {
+                                setState(() {
+                                  schools = searchSchoolcountries!
+                                      .where((element) => element.country
+                                          .toString()
+                                          .toLowerCase()
+                                          .contains(value.toLowerCase()))
+                                      .where((element) => element.name
+                                          .toString()
+                                          .toLowerCase()
+                                          .contains(value.toLowerCase()))
+                                      .toList();
+                                });
+                                // ignore: unused_element
+                              },
+                            ),
+                          );
+                        }),
                       ],
                     ),
-                  ));
-            }));
+                  ),
+                  Visibility(
+                    visible: isLoaded,
+                    child: SizedBox(
+                        height: scHeight(context),
+                        child: ListView.builder(
+                            shrinkWrap: false,
+                            itemCount: schools!.length,
+                            itemBuilder: (BuildContext context, index) {
+                              return Container(
+                                width: scWidth(context),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 5, vertical: 0),
+                                margin: const EdgeInsets.only(
+                                    right: 2, left: 2, top: 4, bottom: 5),
+                                decoration: BoxDecoration(
+                                    color: index == 0
+                                        ? const Color(0xffECF3FD)
+                                        : index == 1
+                                            ? const Color(0xffD1E7CC)
+                                            : index == 2
+                                                ? const Color(0xffFFF9D7)
+                                                : index == 3
+                                                    ? const Color(0xffF5F1FF)
+                                                    : index == 4
+                                                        ? const Color(
+                                                            0xffF1DBE8)
+                                                        : const Color(
+                                                            0xffCAE5E4),
+                                    borderRadius: BorderRadius.circular(10),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          blurRadius: 5,
+
+                                          // spreadRadius: 5,
+                                          color:
+                                              textLightColor.withOpacity(0.4))
+                                    ]),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: const BorderRadius.only(
+                                          topLeft: Radius.circular(8),
+                                          topRight: Radius.circular(8)),
+                                      child: Text(
+                                        schools![index].name,
+                                        style: headline1(),
+                                      ),
+                                    ),
+                                    Text(
+                                      '${schools![index].country},(${schools![index].alphaTwoCode})',
+                                      style: bodyText1(),
+                                    ),
+                                    Link(
+                                      target: LinkTarget.blank,
+                                      uri: Uri.parse(schools![index]
+                                          .webPages[0]
+                                          .toString()),
+                                      builder: (context, followLink) =>
+                                          TextButton(
+                                              onPressed: followLink,
+                                              child: Text(schools![index]
+                                                  .webPages[0]
+                                                  .toString())),
+                                    )
+                                  ],
+                                ),
+                              );
+                            })),
+                  )
+                ],
+              ),
+            )));
   }
 }
